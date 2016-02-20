3b8d45b59b6686f3ed1dfaee132f4f68b3db92dc
wcjohnson11 <williamcouperjohnson@gmail.com>
2016-02-19 21:51:14 -0800

    user persistence

--- Tweetster/User.swift
+++ Tweetster/User.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+var _currentUser: User?
+let currentUserKey = "CurrentUserKey"
+
 class User: NSObject {
     var name: String?
     var screenName: String?
@@ -23,7 +26,39 @@ class User: NSObject {
         tagline = dictionary["description"] as? String
     }
     
-    
+    class var currentUser: User? {
+        get {
+            if _currentUser == nil {
+                let data = NSUserDefaults.standardUserDefaults().objectForKey(currentUserKey) as? NSData
+            if data != nil {
+                do {
+                    let dictionary = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions()) as! NSDictionary
+            _currentUser = User(dictionary: dictionary)
+                } catch {
+                    print("json read error")
+                }
+            }
+        }
+        
+            return _currentUser
+        }
+            
+        set(user) {
+            _currentUser = user
+            
+            if _currentUser != nil {
+                do {
+                    let data = try NSJSONSerialization.dataWithJSONObject(user!.dictionary!, options: NSJSONWritingOptions())
+                       NSUserDefaults.standardUserDefaults().setObject(data, forKey: currentUserKey)
+                } catch {
+                    print("json write error")
+                }
+            } else {
+                NSUserDefaults.standardUserDefaults().setObject(nil, forKey: currentUserKey)
+            }
+            NSUserDefaults.standardUserDefaults().synchronize()
+        }
+    }
 
 
 }
